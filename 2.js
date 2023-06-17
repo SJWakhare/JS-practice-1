849ca41de00397027e3c1ef4cea0792f6f0e0445
SJWakhare <wshivani123@gmail.com>
2023-06-17 18:23:22 +0530

    committed by shivani

---
+++ 2.js
@@ -0,0 +1,58 @@
+
+// console.log('hello'); //command--node 2.js
+
+// let selectedColors = ['red','blue'];
+// selectedColors[2] = 'green';
+// console.log(selectedColors); //[ 'red', 'blue', 'green' ]
+
+// let selectedColors = ['red','blue'];
+// selectedColors[1] = 'green';
+// console.log(selectedColors); //[ 'red', 'green' ]
+
+// let selectedColors = ['red','blue'];
+// selectedColors[3] = 'green';
+// console.log(selectedColors); //[ 'red', 'blue', <1 empty item>, 'green' ]
+
+
+// //function declaration 
+// function greet(name) {
+//     //body of the function
+//     console.log('Hello ' + name);
+// }
+// //function call
+// greet('SJ');
+
+
+// function square(num) {
+//    return num*num;
+// }
+// let num = square(2);
+// console.log('Square is ' + num);
+
+
+//String primitive
+const message = 'This is my message';
+
+//String object
+const another = new String('hi');
+// // message.length
+// // 18
+// // message[0]
+// // "T"
+// // message.includes ('is')
+// // true
+// // message.includes ('not')
+// // false
+// // message.indexOf('i')
+// // 2 
+
+// let hour = 13;
+// if(hour>=6 && hour< 12){
+//     console.log('Good morning');
+// }
+// else if(hour >= 12 && hour < 16){
+//     console.log('Good afternoon');
+// }
+// else{
+//     console.log('Good night');
+// }
---
+++ script1.js
@@ -0,0 +1,60 @@
+/*let js = 'amazing';
+        if(js=== 'amazing') alert('Javascript is fun');
+        40+60-1; //doesn't give any output
+        console.log(40 + 60 - 1); 
+        //keep space in operator and operand
+*/
+
+
+/*
+console.log(40 + 60 - 1);
+console.log('Shivani');
+console.log(20);
+
+let firstName = "Wakhare";
+console.log(firstName);
+
+let myFirstJob = "HR";
+let myCurrentJob = "Programmer";
+console.log(myFirstJob);
+
+let PI = 3.14;
+console.log(PI);
+*/
+
+
+/*
+true;
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Shivani');
+
+javascriptIsFun = 'Yes';
+console.log(javascriptIsFun);
+console.log(typeof 'javascriptIsFun');
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+
+let age = 30;
+age = 31;
+
+const birthYear = 1993;
+//birthYear = 1998;  //Uncaught TypeError: invalid assignment to const 'birthYear'
+
+//const job; //Uncaught SyntaxError: missing = in const declaration
+
+
+
